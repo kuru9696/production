7022f354d3bc04946cce94ebf1473496efffb8b6
kuru9696 <kurumi.inao@gmail.com>
2026-07-15 12:12:04 +0000

    pythonを修正

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('choose.html')
 
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
